704d876c32d65d351ac68a38a2ec3afe2d3e3db4
Fabio Fiorucci Filho <ffiorucci.filho@gmail.com>
2025-01-15 18:38:57 -0300

    corrigindo erro de não aparecer os numeros na tela

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,7 +1,7 @@
 function sortear(){
-    let quantidade = parseInt(document.getElementById('quantidade')).value;
-    let de = parseInt(document.getElementById('de')).value;
-    let ate = parseInt(document.getElementById('ate')).value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
     if (de >= ate) {
         alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
